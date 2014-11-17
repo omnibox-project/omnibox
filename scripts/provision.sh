e3c04729de6f4aaaa05c0b9739121ffdae99c417
jens.paulin <jens.paulin@familjelivmedia.se>
2014-11-17 23:46:49 +0100

    add logs dir to git

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -3,9 +3,6 @@
 # Install less
 npm install -g less
 
-# Create logs directory
-mkdir -p /vagrant/logs
-
 # Configure MySQL Access
 mysql --user="root" --password="secret" -e "UPDATE mysql.user SET Password='' WHERE User='root';" 2>/dev/null
 mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;" 2>/dev/null
