559ae0da4d2888e460442fd1f1c67a5c33c7bbaa
Olcay <olcay@konuskan.se>
2014-11-17 23:32:16 +0100

    cleanup box between provisions

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -13,3 +13,10 @@ mysql --user="root" --password="" -e "FLUSH PRIVILEGES;" 2>/dev/null
 mysql --user="root" --password="" -e "UPDATE mysql.user SET Password='' WHERE User='root';" 2>/dev/null
 mysql --user="root" --password="" -e "FLUSH PRIVILEGES;" 2>/dev/null
 service mysql reload
+
+# Remove old nginx configurations
+rm -R -f /etc/nginx/sites-available/*
+rm -R -f /etc/nginx/sites-enabled/*
+
+# Remove empty directories left over from removed projects
+find /home/vagrant/. -depth -type d -empty -exec rmdir "{}" \;
