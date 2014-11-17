28eca6024602694c6f341a203757068278a6f803
jens.paulin <jens.paulin@familjelivmedia.se>
2014-11-17 23:18:48 +0100

    set root password for mysql db to empty string

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -5,3 +5,11 @@ npm install -g less
 
 # Create logs directory
 mkdir -p /vagrant/logs
+
+# Configure MySQL Access
+mysql --user="root" --password="secret" -e "UPDATE mysql.user SET Password='' WHERE User='root';" 2>/dev/null
+mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;" 2>/dev/null
+mysql --user="root" --password="" -e "FLUSH PRIVILEGES;" 2>/dev/null
+mysql --user="root" --password="" -e "UPDATE mysql.user SET Password='' WHERE User='root';" 2>/dev/null
+mysql --user="root" --password="" -e "FLUSH PRIVILEGES;" 2>/dev/null
+service mysql reload
