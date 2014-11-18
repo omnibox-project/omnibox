1c80352d48ef0507b6414204d980c41374f04ab0
jens.paulin <jens.paulin@familjelivmedia.se>
2014-11-18 02:17:26 +0100

    refactor checkConfig

--- src/Uberstead/BaseCommand.php
+++ src/Uberstead/BaseCommand.php
@@ -13,6 +13,56 @@ use Symfony\Component\Console\Question\Question;
 
 class BaseCommand extends Command
 {
+    protected function checkConfig(InputInterface $input, OutputInterface $output)
+    {
+        if (posix_getuid() != 0) {
+            $output->writeln('This command needs access to /etc/hosts and /etc/exports. Please run as root (sudo).');
+            die();
+        }
+
+        exec('echo ~', $out);
+        if (!file_exists($out[0].'/.ssh/id_rsa.pub')) {
+            $output->writeln('It seems like you don\'t have any SSH keys. Run <question>ssh-keygen -t rsa -C "your_email@example.com"</question> to generate keys.');
+            die();
+        }
+
+        $filename = 'uberstead.yaml';
+        if (!file_exists($filename)) {
+
+            $helper = $this->getHelper('question');
+            $question = new ConfirmationQuestion('<question>uberstead.yaml does not exist! Would you like to generate it? [y]</question>', true);
+
+            $output->writeln('');
+
+            if ($helper->ask($input, $output, $question)) {
+                $question = new Question('Which IP would you like to assign to the server? [192.168.10.10]: ', '192.168.10.10');
+                $ip = $helper->ask($input, $output, $question);
+                $question = new Question('Amount of memory [2048]: ', '2048');
+                $memory = $helper->ask($input, $output, $question);
+                $question = new Question('Number of CPU cores [1]: ', '1');
+                $cpu = $helper->ask($input, $output, $question);
+            }
+
+            $yaml = <<<EOF
+ip: "{$ip}"
+memory: {$memory}
+cpus: {$cpu}
+
+authorize: ~/.ssh/id_rsa.pub
+
+keys:
+    - ~/.ssh/id_rsa
+
+defaultfoldertype: nfs
+
+sites:
+EOF;
+            file_put_contents($filename, $yaml);
+            chmod($filename, 0664);
+            chown($filename, end(explode('/', $out[0])));
+        }
+    }
+
     public function setDbHintInParametersYml($directory, $name, $ip)
     {
         $name = str_replace(" ", "_", $name);
@@ -56,13 +106,6 @@ class BaseCommand extends Command
             $fileContents = trim($fileContents, "\n")."\n";
             file_put_contents($parametersYml, $fileContents);
         }
-    }
-
-    public function checkConfig(InputInterface $input, OutputInterface $output)
-    {
-        $command = new CheckConfigCommand();
-        $command->setApplication($this->getApplication());
-        $command->run($input, $output);
     }
 
     protected function getConfig()
