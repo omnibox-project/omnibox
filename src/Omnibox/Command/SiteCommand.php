9bea961d896345ce97bc5b4a23fabfca9dd5f2fa
Olcay <olcay@konuskan.se>
2014-11-23 00:41:37 +0100

    added support for passing arguments to subcommands

--- src/Omnibox/Command/SiteCommand.php
+++ src/Omnibox/Command/SiteCommand.php
@@ -2,6 +2,7 @@
 namespace Omnibox\Command;
 
 use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -13,6 +14,7 @@ class SiteCommand extends BaseCommand
             ->setName('site')
             ->setDescription('Manages sites: add, remove, list')
             ->addArgument('subcommand')
+            ->addArgument('arguments', InputArgument::IS_ARRAY)
             ->requiresRootAccess()
             ->setSubcommands(
                 array(
